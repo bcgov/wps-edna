e2d119c37499d476c70d018aade8fe628b11d4c6
Andrea Williams <andrea.williams@gov.bc.ca>
2022-01-07 16:19:48 -0800

    Download of free EC obs data into folder works

--- README.md
+++ README.md
@@ -4,5 +4,11 @@ Esoteric Data Nabber Automated. Python tool to automate fetching datasets from v
 ## To Run
 
 ```bash
-python3 main.py fetch_data --destination_folder='<destination-folder>'
+python3 main.py fetch_data --folder='<destination-folder>'
 ```
+
+## Misc.
+
+Edna says:
+
+<iframe src="https://giphy.com/embed/dePaPOPNSLDsk" width="480" height="201" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/the-incredibles-edna-mode-dePaPOPNSLDsk">via GIPHY</a></p>
--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 from typing import Dict, List
 import requests
+import html5lib
+from bs4 import BeautifulSoup
 import os
+import sys
+from pathlib import Path
 import fire
-
 
 class DataSource:
     def __init__(self, url, credentials, friendly_name) -> None:
@@ -13,32 +16,44 @@ class DataSource:
         self.friendly_name = friendly_name
 
 
-destination_folder = None
 sources: List[DataSource] = []
 
-default_sources = [DataSource(url='https://dd.weather.gc.ca/vertical_profile/observation/csv/ObsTephi_00_CAWE.csv', friendly_name='EC Vertical Profile', credentials=None)]
+default_sources = [DataSource(url='https://dd.weather.gc.ca/vertical_profile/observation/csv/', friendly_name='EC Vertical Profile', credentials=None)]
 sources += default_sources
 
-def fetch_data():
+def fetch_data(folder):
     for source in sources:
         print('Fetching data from {}'.format(source.url))
         response = requests.get(source.url)
-        success = None
-        if response.status_code != 200:
-            print('Encountered error fetching {} - error code {}'.format(source.url, response.status_code))
-            success = False
-        data = response.text
-        if len(data) == 0:
-            print('Could not retrieve any data from {}'.format(source.url))
-            success = False
-        else:
-            print('Pulled data from {}. Status code {}'.format(source.url, response.status_code))
-            success = True
+        soup = BeautifulSoup(response.content, 'html5lib')
+        html_list = soup.find_all('a')
+        source_files = []
+        for row in html_list:
+            file_url = str(row['href'])
+            if file_url.endswith('.csv'):
+                source_files.append(file_url)
+        for filename in source_files:
+            url = os.path.join(source.url, filename)
+            success = None
+            download_response = requests.get(url)
+            if download_response.status_code != 200:
+                print('Encountered error fetching {} - error code {}'.format(url, download_response.status_code))
+                success = False
+            data = download_response.text
+            if len(data) == 0:
+                print('Could not retrieve any data from {}'.format(url))
+                success = False
+            else:
+                print('Pulled data from {}. Status code {}'.format(url, download_response.status_code))
+                success = True
 
-        if success is True:
-            with open(os.path.join(destination_folder, source.friendly_name+'.csv'), 'w+') as dest_file:
-                print('Writing data to destination file {}'.format(dest_file))
-                dest_file.write(data)
+            if success is True:
+                Path(os.path.join(folder, source.friendly_name)).mkdir(parents=True, exist_ok=True)
+                dest_filepath = os.path.join(folder, source.friendly_name, filename)
+                print('Saving to {}'.format(dest_filepath))
+                with open(dest_filepath, 'w+') as dest_file:
+                    print('Writing data to destination file {}'.format(dest_file))
+                    dest_file.write(data)
 
     print('Finished retrieving all datasets')
 
